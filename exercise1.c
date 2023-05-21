f0139a0a5016ad8e724f2da9b7bb036f24af0682
defalt <130259647+theonedefalt@users.noreply.github.com>
2023-05-20 23:51:47 -0300

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fill_grades(float grade[], int size) {
-printf("Enter the grades of %d students: ");
-for (int i = 0; i < size; i++) {
-scanf("%f", &grade[i]);
-}
-}
-
-void calculate_average(float grade[], int size) {
-int count = 0;
-float average = 0, sum = 0;
-for (int j = 0; j < size; j++) {
-sum += grade[j];
+void fill_grades(float grade[], int size)
+{
+    printf("Enter the grades of %d students: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%f", &grade[i]);
+    }
 }
 
-average = sum / size;
+void calculate_average(float grade[], int size)
+{
+    int count = 0;
+    float average = 0, sum = 0;
+    for (int j = 0; j < size; j++)
+    {
+        sum += grade[j];
+    }
 
-for (int j = 0; j < size; j++) {
-if (grade[j] > average) {
-count++;
+    average = sum / size;
+
+    for (int j = 0; j < size; j++)
+    {
+        if (grade[j] > average)
+        {
+            count++;
+        }
+    }
+    printf("The class average is: %.2f\n", average);
+    printf("The count result is: %d\n", count);
 }
-}
-printf("The class average is: %.2f\n", average);
-printf("The count result is: %d\n", count);
-}
 
-int main(void) {
-float grade[10];
+int main(void)
+{
+    float grade[10];
 
-fill_grades(grade, 10);
-calculate_average(grade, 10);
+    fill_grades(grade, 10);
+    calculate_average(grade, 10);
 
-return 0;
+    return 0;
 }
